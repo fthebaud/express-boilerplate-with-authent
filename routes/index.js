b3c9738417c41502d4d4eada7f9de45dd40d1a9c
Fabien Thebaud <fthebaud@gmail.com>
2017-05-07 12:34:17 +0200

    Much cleaner solution using URL redirection

--- routes/index.js
+++ routes/index.js
@@ -3,49 +3,73 @@ const path = require('path');
 
 const router = express.Router();
 
+
+// top secret password that gives access to the page
 const password = 'motmagique';
 
-// generate token for cookie
+// Token (will be stored in a cookie)
 const token = '45096213712045796';
 
-function sendIndexPage(res) {
-  res.sendFile('index/html/index.html', { root: path.resolve(__dirname) });
-}
 
-function sendPasswordPage(res, msg) {
-  res.render('index/views/password', { message: msg ? msg : ''});
-}
-
-
-/* GET home page. */
+/* GET / */
 router.get('/', function (req, res, next) {
-  const { userToken } = req.cookies;
-  if (!userToken || userToken !== token){
-    // No token or bad token : ask for password
-    sendPasswordPage(res, 'Please enter your password');
-    return;
-  }
-  // token OK : send back index page
-  sendIndexPage(res);
+  res.redirect('/index');
 });
 
 
-/* POST : check password  */
-router.post('/', function (req, res, next) {
-  // bad password : ask for password again
+/* GET /index */
+router.get('/index', function (req, res, next) {
+  // check if user is authenticated by looking at token in cookie
+  if (isTokenValid(req.cookies.userToken)){
+    // token OK : send back index page
+    res.sendFile('index/html/index.html', { root: path.resolve(__dirname) });
+  } else {
+    // No token or bad token : redirect to password page
+    res.redirect('/password');
+  }
+});
+
+
+/* GET /password */
+router.get('/password', skipPassword, function (req, res, next) {
+  let msg = '<div>Entrer votre mdp</div>';
+  if (req.query.loginFailed) {
+    msg = '<div class="error">erreur, mot de passe incorrect</div>';
+  }
+  res.render('index/views/password', { message: msg});
+});
+
+
+/* POST /password  */
+router.post('/password', function (req, res, next) {
+  // check password
   if (req.body.userPassword !== password) {
-    sendPasswordPage(res, 'erreur, mot de passe incorrect');
-    return;
+    // bad password : redirect to password page with a warning
+    res.redirect('/password?loginFailed=true');
+  } else {
+    // correct password : we add cookie...
+    res.cookie('userToken', token, {
+      maxAge: 604800, // 7 jours (en secondes)
+      httpOnly: true
+      /* , secure: true*/
+    });
+    // ...and we redirect to the index page
+    res.redirect('/index');
   }
-  // password OK : we add cookie
-  res.cookie('userToken', token, {
-    maxAge: 604800, // 7 jours (en secondes)
-    httpOnly: true
-    /* , secure: true*/
-  });
-  // and we send back index page
-  sendIndexPage(res);
 });
+
+function skipPassword(req, res, next) {
+  // is the user is already authenticated, skip directly to index page
+  if (isTokenValid(req.cookies.userToken)) {
+    res.redirect('/index');
+  } else {
+    next();
+  }
+}
+
+function isTokenValid(userToken) {
+  return (userToken && userToken === token);
+}
 
 
 module.exports = router;
